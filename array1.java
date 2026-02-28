2b3b4e6ce3ac498a506f377ad2e452bca6d4875e
Kyle-gi <hyrokai.oliver@gmail.com>
2026-02-28 13:32:53 +0100

    changes

--- array1.java
+++ array1.java
@@ -1,26 +1,28 @@
 package JavaExercises;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class array1 {
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+
         System.out.println("insertar 5 numeros!");
 
-        int[] numeros = new int[5];
+        int[] vector = new int[15];
         int sum = 0;
 
-        for (int i = 0; i < numeros.length; i++) {
+        for (int i = 0; i < vector.length; i++) {
             System.out.print("Introduce el siguente numero " + (i + 1) + ": ");
-            numeros[i] = sc.nextInt();
+            vector[i] = sc.nextInt();
         }
 
         System.out.println("\n=== Results ===");
         System.out.print("Introduced numbers: ");
-		for (int i = 0; i < numeros.length; i++){
-            System.out.print(numeros[i] + "  ");
-            sum += numeros[i];
+		for (int i = 0; i < vector.length; i++){
+            System.out.print(vector[i] + "  ");
+            sum += vector[i];
 		}
         System.out.println("\n");
         System.out.print("The total sum of these numbers is: "  + sum);
---
+++ array2.java
@@ -0,0 +1,8 @@
+package JavaExercises;
+
+public class array2 {
+    public static void main(String[] args){
+        
+        int[] destino = new int[25];
+    }
+}
