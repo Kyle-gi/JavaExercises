23cfdd0ee1e5df7c41512f26b36ce571a834e381
Your Name <Kyle-gi@users.noreply.github.com.>
2026-03-01 18:35:33 +0100

    changes

--- array2.java
+++ array2.java
@@ -1,8 +1,15 @@
 package JavaExercises;
 
 public class array2 {
-    public static void main(String[] args){
-        
-        int[] destino = new int[25];
+    public static void main(String[] args) {
+
+        int[] origen = {10, 25, 26, 40, 37, 45, 90, 76, 78, 81, 58, 32};
+
+        for (int i = 0; i < origen.length; i++) {
+            if (origen[i] % 2 == 0) {
+                System.out.print("Son numeros pares: " + origen[i]);
+                System.out.println();
+            }
+        }
     }
 }
