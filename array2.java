4fa025dd7556b4e4fba9ea2647f9ad8af9ccd547
Kyle-gi <hyrokai.oliver@gmail.com>
2026-03-02 10:10:00 +0100

    changes

--- array2.java
+++ array2.java
@@ -1,32 +1,46 @@
 package JavaExercises;
 
+import java.util.Arrays;
+
 public class array2 {
     public static void main(String[] args) {
 
         int[] origen = {10, 25, 26, 40, 37, 45, 90, 76, 78, 81, 58, 32};
         int[] destino = new int[origen.length];
 
+        System.out.print("Son numeros pares: ");
         for (int i = 0; i < origen.length; i++) {
             if (origen[i] % 2 == 0) {
-                System.out.print("Son numeros pares: " + origen[i]);
-                System.out.println();
+                System.out.print(origen[i]+", ");
             }
+        }
+        System.out.println();
+
+        System.out.print("Son numeros impares: ");
+        for (int i = 0; i < origen.length; i++) {
             if (origen[i] % 2 != 0){
-                System.out.print("Son numeros impares: " + origen[i]);
-                System.out.println();
+                System.out.print(origen[i]+", ");
             }
         }
+        System.out.println();
 
-        for (int i = 0; i < destino.length; i++){
-            if (destino[i] % 2 == 0 && destino[i] > 25){
-                System.out.print("Destino de numeros pares: " + destino[i]);
-                System.out.println();
-            }
-            if (destino[i] % 2 != 0 && destino[i] > 25){
-                System.out.print("Destino de numeros impares: " + destino[i]);
-                System.out.println();
+        System.out.print("Destino de numeros pares: ");
+        for (int i = 0; i < origen.length; i++) {
+            if (origen[i] % 2 == 0 && origen[i] > 25) {
+                destino[i] = origen[i];
+                System.out.print(destino[i]+", ");
             }
         }
+        System.out.println();
 
+
+        System.out.print("Destino de numeros impares: ");
+        for (int i = 0; i < origen.length; i++) {
+            if (origen[i] % 2 != 0 && origen[i] > 25) {
+                destino[i] = origen[i];
+                System.out.print(destino[i] + ", ");
+            }
+        }
+        System.out.println();
     }
 }
