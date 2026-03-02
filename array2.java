223065f7a85018e4982dfbb42a21b62625233bce
Kyle-gi <hyrokai.oliver@gmail.com>
2026-03-02 09:11:32 +0100

    changes
    # Conflicts:
    #	array2.java
    #	out/production/JavaExercises/JavaExercises/array2.class

--- array2.java
+++ array2.java
@@ -4,12 +4,30 @@ public class array2 {
     public static void main(String[] args) {
 
         int[] origen = {10, 25, 26, 40, 37, 45, 90, 76, 78, 81, 58, 32};
+        int[] destino = new int[origen.length];
 
         for (int i = 0; i < origen.length; i++) {
             if (origen[i] % 2 == 0) {
                 System.out.print("Son numeros pares: " + origen[i]);
                 System.out.println();
             }
+            if (origen[i] % 2 != 0){
+                System.out.print("Son numeros impares: " + origen[i]);
+                System.out.println();
+            }
         }
+
+
+        for (int i = 0; i < destino.length; i++){
+            if (destino[i] % 2 == 0 && destino[i] > 25){
+                System.out.print("Destino de numeros pares: " + destino[i]);
+                System.out.println();
+            }
+            if (destino[i] % 2 != 0 && destino[i] > 25){
+                System.out.print("Destino de numeros impares: " + destino[i]);
+                System.out.println();
+            }
+        }
+
     }
 }
