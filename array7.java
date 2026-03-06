5bf26a89a02749d22297a29089a8cb8fbdfe3a4c
_kyleofx_ <Kyle-gi@users.noreply.github.com>
2026-03-06 14:48:44 +0100

    changes

---
+++ array7.java
@@ -0,0 +1,8 @@
+package JavaExercises;
+
+public class array7 {
+    public static void main(String[] args){
+
+        int [] numeros = {1, 33, 12, 22, 3, 4, 5, 55, 66, 80, 81, 14, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 ,1}
+    }
+}
